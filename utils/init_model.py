a07256d6d794c0767bd3e4f7238949959794761b
varenick <golikov.e.a000@gmail.com>
2019-11-19 19:24:05 +0300

    add: experiments with deep cnns

--- utils/init_model.py
+++ utils/init_model.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 import torch
+import torch.nn as nn
 import torch.optim as optim
 
 from itertools import chain
@@ -13,6 +14,14 @@ def _test_gradient_normalization(optimizer):
         return True
     else:
         raise ValueError("Unsupported optimizer: {}".format(type(optimizer)))
+        
+        
+def _count_layers(layers):
+    counter = 0
+    for layer in layers:
+        if isinstance(layer, (nn.Linear, nn.Conv2d)):
+            counter += 1
+    return counter
 
 
 def scale_hyperparams(input_layer, hidden_layers, output_layer, 
@@ -22,44 +31,66 @@ def scale_hyperparams(input_layer, hidden_layers, output_layer,
     #assert optimizer.param_groups[0]['params'] == input_layer.parameters(), "the first param group of optimizer should correspond to input layer parameters"
     
     all_except_input_layers = chain(hidden_layers, [output_layer])
+    #hidden_layer_count = _count_layers(hidden_layers)
     
     is_gradient_normalized = _test_gradient_normalization(optimizer)
     
+    use_bn = False
+    for layer in hidden_layers:
+        if isinstance(layer, (nn.BatchNorm1d, nn.BatchNorm2d)):
+            use_bn = True
+            break
+    
     if scaling_mode == 'default':
         weight_factor = 1
-        lr_factor = 1
-        lr_factor_input = 1. / lr_factor
+        lr_factor_input = 1
+        lr_factor_hidden = 1
+        lr_factor_output = 1
     
     elif scaling_mode == 'preserve_initial_logit_std':
+        if hidden_layer_count > 0 or use_bn:
+            raise NotImplementedError
         weight_factor = 1
-        lr_factor = width_factor ** (-0.5)
-        lr_factor_input = 1. / lr_factor
+        lr_factor_input = width_factor ** (0.5)
+        lr_factor_hidden = width_factor ** (-0.5)
+        lr_factor_output = width_factor ** (-0.5)
         
     elif scaling_mode == 'preserve_logit_mean':
         weight_factor = width_factor ** (-0.5)
-        lr_factor = width_factor ** (-1.)
-        lr_factor_input = 1. / lr_factor
+        if is_gradient_normalized:
+            lr_factor_input = 1
+            lr_factor_hidden = width_factor ** (-1)
+            lr_factor_output = width_factor ** (-1)
+        else:
+            if use_bn:
+                lr_factor_input = 1
+                lr_factor_hidden = width_factor ** (-2)
+            else:
+                lr_factor_input = width_factor
+                lr_factor_hidden = 1
+            lr_factor_output = width_factor ** (-1)
         
     elif scaling_mode == 'preserve_initial_logit_std_and_logit_mean':
+        if hidden_layer_count > 0 or use_bn:
+            raise NotImplementedError
         weight_factor = 1
-        lr_factor = width_factor ** (-0.5)
-        #lr_factor_input = width_factor ** 0.5
-        lr_factor_input = 1. / lr_factor
+        lr_factor_input = width_factor ** (0.5)
+        lr_factor_hidden = width_factor ** (-0.5)
+        lr_factor_output = width_factor ** (-0.5)
         
     else:
         raise ValueError("Unknown scaling mode: {}".format(scaling_mode))
         
     for layer in all_except_input_layers:
-        if hasattr(layer, 'weight'):
+        if isinstance(layer, (nn.Linear, nn.Conv2d)):
             layer.weight.data = layer.weight.data * weight_factor
-        if hasattr(layer, 'bias'):
-            if layer.bias is not None:
-                raise NotImplementedError
+            if hasattr(layer, 'bias'):
+                if layer.bias is not None:
+                    raise NotImplementedError
                 
-    if not is_gradient_normalized:
-        for param_group in optimizer.param_groups[:1]:
-            param_group['lr'] *= lr_factor_input
+    assert len(optimizer.param_groups) == 3
                 
-    for param_group in optimizer.param_groups[1:]:
-        param_group['lr'] *= lr_factor
+    optimizer.param_groups[0]['lr'] *= lr_factor_input
+    optimizer.param_groups[1]['lr'] *= lr_factor_hidden
+    optimizer.param_groups[2]['lr'] *= lr_factor_output
     
--- utils/models.py
+++ utils/models.py
@@ -28,3 +28,48 @@ class FCNet(nn.Module):
         Z = self.input_layer(X.view(X.shape[0], -1))
         Z = self.hidden_layers(Z)
         return self.output_layer(Z.view(Z.shape[0], -1))
+    
+    
+def make_layers(config, input_shape, init_num_kernels, use_bn, bias, dropout_rate):
+    out_channels, h, w = input_shape
+    num_kernels = init_num_kernels
+    layers = []
+    for layer_name in config:
+        if layer_name == 'conv':
+            in_channels, out_channels = out_channels, num_kernels
+            layers += [nn.Conv2d(in_channels, out_channels, 3, padding=1, bias=bias), nn.LeakyReLU(0.1)]
+            if use_bn:
+                layers += [nn.BatchNorm2d(out_channels)]
+        elif layer_name == 'max-pool':
+            layers += [nn.MaxPool2d(2), nn.Dropout2d(dropout_rate)]
+            num_kernels *= 2
+            h = (h+1) // 2
+            w = (w+1) // 2
+        elif layer_name == 'avg-pool':
+            layers += [nn.AvgPool2d((h,w)), nn.Flatten()]
+        elif layer_name == 'dense':
+            num_kernels //= 2
+            in_channels, out_channels = out_channels, num_kernels
+            layers += [nn.Linear(in_channels, out_channels, bias=bias), nn.LeakyReLU(0.1)]
+            if use_bn:
+                layers += [nn.BatchNorm1d(out_channels, affine=False)]
+                
+    return layers, out_channels
+
+    
+class ConvLarge(nn.Module):
+    def __init__(self, input_shape, num_classes, init_num_kernels, use_bn=True, hidden_bias=False, output_bias=True, dropout_rate=0.5,
+                 config=('conv', 'conv', 'max-pool', 'conv', 'conv', 'max-pool', 'conv', 'avg-pool', 'dense')):
+        super(ConvLarge, self).__init__()
+        
+        layers, out_features = make_layers(config, input_shape, init_num_kernels, use_bn, hidden_bias, dropout_rate)
+        
+        self.input_layer = layers[0]
+        self.hidden_layers = nn.Sequential(*layers[1:])
+        
+        self.output_layer = nn.Linear(out_features, num_classes, bias=output_bias)
+        
+    def forward(self, X):
+        Z = self.input_layer(X)
+        Z = self.hidden_layers(Z)
+        return self.output_layer(Z)
