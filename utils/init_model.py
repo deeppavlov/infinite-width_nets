892058f9a662ae26865471f55a0144ad387609db
varenick <golikov.e.a000@gmail.com>
2019-12-09 11:25:15 +0300

    experiments with conv-large bn

--- utils/init_model.py
+++ utils/init_model.py
@@ -27,9 +27,6 @@ def _count_layers(layers):
 def scale_hyperparams(input_layer, hidden_layers, output_layer, 
                       optimizer, width_factor, scaling_mode):
     
-    #assert len(optimizer.param_groups) > 1, "there should be at least two param groups in optimizer; the first one should correspond to input layer"
-    #assert optimizer.param_groups[0]['params'] == input_layer.parameters(), "the first param group of optimizer should correspond to input layer parameters"
-    
     all_except_input_layers = chain(hidden_layers, [output_layer])
     #hidden_layer_count = _count_layers(hidden_layers)
     
@@ -47,13 +44,16 @@ def scale_hyperparams(input_layer, hidden_layers, output_layer,
         lr_factor_hidden = 1
         lr_factor_output = 1
     
-    elif scaling_mode == 'preserve_initial_logit_std':
-        if hidden_layer_count > 0 or use_bn:
-            raise NotImplementedError
+    elif scaling_mode in ['preserve_initial_logit_std', 'preserve_initial_logit_std_and_logit_mean']:
         weight_factor = 1
-        lr_factor_input = width_factor ** (0.5)
-        lr_factor_hidden = width_factor ** (-0.5)
-        lr_factor_output = width_factor ** (-0.5)
+        if is_gradient_normalized:
+            lr_factor_input = 1
+            lr_factor_hidden = width_factor ** (-0.5)
+            lr_factor_output = width_factor ** (-0.5)
+        else:
+            lr_factor_input = width_factor ** 0.5
+            lr_factor_hidden = 1
+            lr_factor_output = width_factor ** (-0.5)
         
     elif scaling_mode == 'preserve_logit_mean':
         weight_factor = width_factor ** (-0.5)
@@ -62,21 +62,63 @@ def scale_hyperparams(input_layer, hidden_layers, output_layer,
             lr_factor_hidden = width_factor ** (-1)
             lr_factor_output = width_factor ** (-1)
         else:
-            if use_bn:
-                lr_factor_input = 1
-                lr_factor_hidden = width_factor ** (-2)
-            else:
-                lr_factor_input = width_factor
-                lr_factor_hidden = 1
+            lr_factor_input = width_factor
+            lr_factor_hidden = 1
             lr_factor_output = width_factor ** (-1)
         
-    elif scaling_mode == 'preserve_initial_logit_std_and_logit_mean':
-        if hidden_layer_count > 0 or use_bn:
-            raise NotImplementedError
+    else:
+        raise ValueError("Unknown scaling mode: {}".format(scaling_mode))
+        
+    for layer in all_except_input_layers:
+        if isinstance(layer, (nn.Linear, nn.Conv2d)):
+            layer.weight.data = layer.weight.data * weight_factor
+            if hasattr(layer, 'bias'):
+                if layer.bias is not None:
+                    raise NotImplementedError
+                
+    assert len(optimizer.param_groups) == 3
+                
+    optimizer.param_groups[0]['lr'] *= lr_factor_input
+    optimizer.param_groups[1]['lr'] *= lr_factor_hidden
+    optimizer.param_groups[2]['lr'] *= lr_factor_output
+
+
+def correct_hyperparams(input_layer, hidden_layers, output_layer, 
+                        epoch, optimizer, width_factor, scaling_mode, correction_epoch=0):
+    
+    all_except_input_layers = chain(hidden_layers, [output_layer])
+    #hidden_layer_count = _count_layers(hidden_layers)
+    
+    is_gradient_normalized = _test_gradient_normalization(optimizer)
+    
+    use_bn = False
+    for layer in hidden_layers:
+        if isinstance(layer, (nn.BatchNorm1d, nn.BatchNorm2d)):
+            use_bn = True
+            break
+    
+    if scaling_mode == 'preserve_initial_logit_std_and_logit_mean':
+        if epoch == correction_epoch:
+            weight_factor = width_factor ** (-0.5)
+            if is_gradient_normalized:
+                lr_factor_input = 1
+                lr_factor_hidden = width_factor ** (-0.5)
+                lr_factor_output = width_factor ** (-0.5)
+            else:
+                lr_factor_input = width_factor ** 0.5
+                lr_factor_hidden = 1
+                lr_factor_output = width_factor ** (-0.5)
+        else:
+            weight_factor = 1
+            lr_factor_input = 1
+            lr_factor_hidden = 1
+            lr_factor_output = 1
+    
+    elif scaling_mode in ['preserve_initial_logit_std', 'preserve_logit_mean', 'default']:
         weight_factor = 1
-        lr_factor_input = width_factor ** (0.5)
-        lr_factor_hidden = width_factor ** (-0.5)
-        lr_factor_output = width_factor ** (-0.5)
+        lr_factor_input = 1
+        lr_factor_hidden = 1
+        lr_factor_output = 1
         
     else:
         raise ValueError("Unknown scaling mode: {}".format(scaling_mode))
--- utils/models.py
+++ utils/models.py
@@ -30,7 +30,7 @@ class FCNet(nn.Module):
         return self.output_layer(Z.view(Z.shape[0], -1))
     
     
-def make_layers(config, input_shape, init_num_kernels, use_bn, bias, dropout_rate):
+def make_layers(config, input_shape, init_num_kernels, use_bn, bias, dropout_rate, bn_affine):
     out_channels, h, w = input_shape
     num_kernels = init_num_kernels
     layers = []
@@ -39,7 +39,7 @@ def make_layers(config, input_shape, init_num_kernels, use_bn, bias, dropout_rat
             in_channels, out_channels = out_channels, num_kernels
             layers += [nn.Conv2d(in_channels, out_channels, 3, padding=1, bias=bias), nn.LeakyReLU(0.1)]
             if use_bn:
-                layers += [nn.BatchNorm2d(out_channels)]
+                layers += [nn.BatchNorm2d(out_channels, affine=bn_affine)]
         elif layer_name == 'max-pool':
             layers += [nn.MaxPool2d(2), nn.Dropout2d(dropout_rate)]
             num_kernels *= 2
@@ -52,17 +52,17 @@ def make_layers(config, input_shape, init_num_kernels, use_bn, bias, dropout_rat
             in_channels, out_channels = out_channels, num_kernels
             layers += [nn.Linear(in_channels, out_channels, bias=bias), nn.LeakyReLU(0.1)]
             if use_bn:
-                layers += [nn.BatchNorm1d(out_channels, affine=False)]
+                layers += [nn.BatchNorm1d(out_channels, affine=bn_affine)]
                 
     return layers, out_channels
 
     
 class ConvLarge(nn.Module):
-    def __init__(self, input_shape, num_classes, init_num_kernels, use_bn=True, hidden_bias=False, output_bias=True, dropout_rate=0.5,
+    def __init__(self, input_shape, num_classes, init_num_kernels, use_bn=True, hidden_bias=False, output_bias=True, dropout_rate=0.5, bn_affine=True,
                  config=('conv', 'conv', 'max-pool', 'conv', 'conv', 'max-pool', 'conv', 'avg-pool', 'dense')):
         super(ConvLarge, self).__init__()
         
-        layers, out_features = make_layers(config, input_shape, init_num_kernels, use_bn, hidden_bias, dropout_rate)
+        layers, out_features = make_layers(config, input_shape, init_num_kernels, use_bn, hidden_bias, dropout_rate, bn_affine)
         
         self.input_layer = layers[0]
         self.hidden_layers = nn.Sequential(*layers[1:])
